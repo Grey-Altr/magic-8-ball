eec544c4c37832308abbc1fc6532e716be8dfb3b
Grey Altr <grey.sifr@gmail.com>
2022-07-21 23:10:09 -0700

    final

--- app.js
+++ app.js
@@ -6,25 +6,20 @@
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
+
+
 const questionSection = document.getElementById('question-section');
-const askedQuestion = document.getElementById('asked-question');
 const askButton = document.getElementById('ask-button');
 const answerSection = document.getElementById('answer-section');
-const questionDisplay = document.getElementById('question-display');
-const answerDisplay = document.getElementById('answer-display');
 const toy = document.getElementById('toy');
+
 
 // set event listeners 
 askButton.addEventListener('click', () => {
-    copyQuestion();
     toggleVisibility();
     applyAnimations();
+    setTimeout(validateBall, 1000);
 });
-
-function copyQuestion() {
-    const question = askedQuestion.value;
-    questionDisplay.textContent = question;
-}
 
 function toggleVisibility() {
     questionSection.classList.add('hidden');
@@ -32,6 +27,10 @@ function toggleVisibility() {
 }
 
 function applyAnimations() {
-    answerDisplay.classList.add('fade-in');
     toy.classList.add('shake');
+}
+
+function validateBall(){
+    const randomId = Math.floor(Math.random() * 20);
+    document.getElementById(randomId).checked = true;
 }
